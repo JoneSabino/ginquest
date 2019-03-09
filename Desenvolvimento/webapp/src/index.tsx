fcfcd24a9e4b535d36ef3b00ba913858361387d5
sp1674706 <sp1674706@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-09 16:46:26 +0000

    Corrigindo bootstrap

--- Desenvolvimento/webapp/src/index.tsx
+++ Desenvolvimento/webapp/src/index.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
