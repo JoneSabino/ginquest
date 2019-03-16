bbdf4cd8e1d8b4ac6de70dbaccd344dca97c9a31
sp167031x <sp167031x@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-16 20:29:03 +0000

    fixed refresh on delete

--- Desenvolvimento/webapp/src/components/QuizList/QuizList.tsx
+++ Desenvolvimento/webapp/src/components/QuizList/QuizList.tsx
@@ -59,7 +59,28 @@ class QuizList extends Component<Props, State> {
             {
                 method: 'DELETE',
             }
-        ).then(() => this.forceUpdate());
+        ).then(() => {
+            fetch('https://ginquest-backend-dot-ginquest-app.appspot.com/quiz')
+                .then(res => res.json())
+                .then(
+                    result => {
+                        console.log(result);
+                        this.setState({
+                            isLoaded: true,
+                            items: result.rows,
+                        });
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    error => {
+                        this.setState({
+                            isLoaded: true,
+                            error,
+                        });
+                    }
+                );
+        });
     }
 
     private renderItem(item: {
