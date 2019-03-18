3ae3fb7080bb19cd860f4daf2760c55147d5950c
sp167031x <sp167031x@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-18 22:50:56 +0000

    Fixed Unit Tests

--- Desenvolvimento/webapp/src/screens/Quiz/Quiz.test.tsx
+++ Desenvolvimento/webapp/src/screens/Quiz/Quiz.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Quiz from './Quiz';
+import { MemoryRouter } from 'react-router';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Quiz />, div);
+    ReactDOM.render(
+        <MemoryRouter>
+            <Quiz />
+        </MemoryRouter>,
+        div
+    );
     ReactDOM.unmountComponentAtNode(div);
 });
