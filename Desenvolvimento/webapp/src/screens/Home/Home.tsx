b11e350896c88119346f93366d55b457b5cdc7eb
sp167031x <sp167031x@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-19 01:10:21 +0000

    Change icons

--- Desenvolvimento/webapp/src/screens/Home/Home.tsx
+++ Desenvolvimento/webapp/src/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/icon512.png';
 import './Home.css';
 import Form from 'react-bootstrap/Form';
 import { withTranslation } from 'react-i18next';
@@ -16,18 +16,6 @@ class Home extends Component<any, any> {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        {t('Edit')} <code>src/App.tsx</code>{' '}
-                        {t('and save to reload')}.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h1>{t('Welcome to React')}</h1>
-                    </a>
                 </header>
             </div>
         );
