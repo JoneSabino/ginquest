9c08d8a9d59f66034816ffe783951fa51fa2de36
sp1674706 <sp1674706@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-23 15:35:17 +0000

    remove branch

--- Desenvolvimento/webapp/branches/j9322/src/screens/Gincana/Gincana.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Gincana from './Gincana';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Gincana />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
