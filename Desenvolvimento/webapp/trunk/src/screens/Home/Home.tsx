c3d34cabc65ba02d3d59858d9be36993b4071728
sp1666339 <sp1666339@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-27 16:25:40 +0000

    Layout Login

--- Desenvolvimento/webapp/trunk/src/screens/Home/Home.tsx
+++ Desenvolvimento/webapp/trunk/src/screens/Home/Home.tsx
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import React, { Component } from 'react';
 import logo from '../../assets/icon512.png';
+import '../../App.css';
 import './Home.css';
 import Form from 'react-bootstrap/Form';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { RouteChildrenProps } from 'react-router';
+import { Button } from 'react-bootstrap';
 
 interface Props extends RouteChildrenProps, WithTranslation {}
 
@@ -18,8 +20,35 @@ class Home extends Component<Props, State> {
     public render() {
         const { t } = this.props;
         return (
-            <div className="App App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+            <div className="App App-header App-Home">
+                <div className="container">
+                    <img src={logo} className="App-logo" alt="GinQuest" />
+
+                    <Form id="Home-Form--Login">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="E-mail" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicChecbox">
+                            <Form.Check type="checkbox" label="Permanecer logado! :)" />
+                        </Form.Group>
+                        <Button variant="secondary" type="submit">
+                            Acessar
+                        </Button>
+                    </Form>
+
+                    <div className="Home-Btn--New">
+                        <a href="" className="btn btn-primary">Sou um novo usuário</a>
+                    </div>
+                    
+                    <div className="Home-Link--Forget">
+                        <a href="">Oooops! Esqueci minha senha!</a>
+                    </div>
+
+                </div>
             </div>
         );
     }
