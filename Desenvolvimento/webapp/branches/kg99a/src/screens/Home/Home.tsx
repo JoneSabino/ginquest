77c35c0a8f204fe3c86c8c1a302b3c8541d00e77
sp1672576 <sp1672576@72fe1d9a-8248-0410-b756-cde062451638>
2019-03-28 23:59:58 +0000

    Login com Facebook

--- Desenvolvimento/webapp/branches/kg99a/src/screens/Home/Home.tsx
+++ Desenvolvimento/webapp/branches/kg99a/src/screens/Home/Home.tsx
@@ -3,20 +3,39 @@ import React, { Component } from 'react';
 import logo from '../../assets/icon512.png';
 import './Home.css';
 import Form from 'react-bootstrap/Form';
+import ReactDOM from 'react-dom';
 import { withTranslation } from 'react-i18next';
+import FacebookLogin from 'react-facebook-login';
 
 class Home extends Component<any, any> {
+    private responseFacebook: any;
+    private componentClicked: any;
     constructor(props: any) {
         super(props);
     }
 
+    // @ts-ignore
     public render() {
         const { t } = this.props;
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
                 </header>
+
+                <div className="facebook-login">
+                    <button className="facebook-button" type="button">
+                        <FacebookLogin
+                            appId="2208482769481221"
+                            autoLoad={true}
+                            fields="name,email,picture"
+                            onClick={this.componentClicked}
+                            callback={this.responseFacebook}/>
+                    </button>
+                </div>
+
+                <div id={"teste"}>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
             </div>
         );
     }
